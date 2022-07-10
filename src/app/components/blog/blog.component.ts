3120b678d4cd423c2b880ed9dadd0df842d94655
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-07-10 22:46:37 +0200

    add styles

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -16,14 +16,14 @@ export class BlogComponent implements OnInit {
     this.noticias = new Array(
       {
         titulo: 'Sunt aut facere repellat provident',
-        imagen: 'https://fakeimg.pl/250x250/',
+        imagen: '../../../assets/images/img1.jpg',
         texto:
           'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
         fecha: '09/07/2022',
       },
       {
         titulo: 'Qui est esse',
-        imagen: 'https://fakeimg.pl/250x250/',
+        imagen: '../../../assets/images/img2.jpg',
         texto:
           'Est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla',
         fecha: '05/07/2022',
